e46fd28995cc44696a9abb67ef528c4dbe947ce7
Ranajit Bera <ranajitbera321@gmail.com>
2026-08-25 21:39:02 +0530

    createList updated

--- LinkedList/createLinkedList.c
+++ LinkedList/createLinkedList.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -9,44 +10,52 @@ typedef struct Node
 
 sNode *createLinkedList(int n);
 void display(sNode *head);
-
+void freeList(sNode *head);
 
 int main()
 {
   int n;
   sNode *head = NULL;
-  printf("Enter the number of the Nodes ");
+  printf("Enter the number of the node ");
   scanf("%d", &n);
+  if (n <= 0)
+  {
+    printf("Invalid node number.");
+    return 1;
+  }
   head = createLinkedList(n);
   display(head);
+  freeList(head);
+
   return 0;
 }
 
 sNode *createLinkedList(int n)
 {
   sNode *head = NULL;
+  sNode *tail = NULL;
   sNode *temp = NULL;
-  sNode *p = NULL;
 
   for (int i = 0; i < n; i++)
   {
-    temp = (sNode *)malloc(sizeof(sNode));
-    printf("Enter the data of Node %d ", i + 1);
-    scanf("%d",&(temp->data));
+    temp = (sNode *)malloc(sizeof(sNode *));
+    if (temp == NULL)
+    {
+      printf("Memory allocation is failed.");
+      exit(0);
+    }
+    printf("Enter the value of the Node %d ", i + 1);
+    scanf("%d", &(temp->data));
     temp->next = NULL;
     if (head == NULL)
     {
       head = temp;
+      tail = temp; // At first head == temp
     }
     else
     {
-      p = head;
-      while (p->next != NULL)
-      {
-        p = p->next;
-      }
-
-      p->next = temp;
+      tail->next = temp; // tail points to the last Node data
+      tail = temp;       // Now this will store the temp value,its pointer move to the last inserted value
     }
   }
   return head;
@@ -55,11 +64,22 @@ sNode *createLinkedList(int n)
 void display(sNode *head)
 {
   sNode *p = head;
-  printf("The list is ");
   while (p != NULL)
   {
-    printf("%d-> ", p->data);
+    printf("[%d|-]--->", p->data);
     p = p->next;
   }
-  printf("NULL");
+  printf(" NULL");
+}
+
+void freeList(sNode *head)
+{
+  sNode *current = head;
+  sNode *nextNode = NULL;
+  while (current != NULL)
+  {
+    nextNode = current->next; // to keep the referance
+    free(current);
+    current = nextNode;
+  }
 }
